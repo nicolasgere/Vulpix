0e293e2ae0fdc369e623e03284f66abc62b3ca57
Filipe GOMES PEIXOTO <gomespeixoto.filipe@gmail.com>
2016-10-21 19:16:55 +0200

    Add Vulpix solution and project file for visual studio

---
+++ Vulpix/src/Vulpix.Test/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace Vulpix.Test
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var app = new VulpixServer();
+            var foo = new MyController();
+
+            app.AddRoute("GET", "/", foo.Index);
+            app.Use(new BodyParser().Exec);
+            app.Listen(5000);
+        }
+        public class MyController
+        {
+            public async void Index(Req req, Res res)
+            {
+                Console.WriteLine("GET: /");
+
+                byte[] response = Encoding.ASCII.GetBytes("Hello world!");
+
+                await res.Response.Body.WriteAsync(response, 0, response.Length);
+            }
+        }
+    }
+}
---
+++ Vulpix/src/Vulpix.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Vulpix.Test")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ef6d1b2e-7565-485b-96ed-8da3e27ac983")]
